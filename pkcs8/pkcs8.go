5ca44aebda30223c2c9722408de945525539bc1e
deatil <2217957370@qq.com>
2022-07-31 13:43:25 +0800

    更新

--- pkcs8/pkcs8.go
+++ pkcs8/pkcs8.go
@@ -3,21 +3,15 @@ package pkcs8
 import (
     "io"
     "fmt"
-    "hash"
     "errors"
-    "crypto/md5"
+    "crypto"
     "crypto/aes"
     "crypto/des"
-    "crypto/sha1"
-    "crypto/sha256"
-    "crypto/sha512"
     "crypto/x509"
     "crypto/x509/pkix"
     "crypto/cipher"
     "encoding/asn1"
     "encoding/pem"
-
-    "golang.org/x/crypto/pbkdf2"
 )
 
 // PBKDF2SaltSize is the default size of the salt for PBKDF2, 128-bit salt.
@@ -26,6 +20,22 @@ const PBKDF2SaltSize = 16
 // PBKDF2Iterations is the default number of iterations for PBKDF2, 100k
 // iterations. Nist recommends at least 10k, 1Passsword uses 100k.
 const PBKDF2Iterations = 10000
+
+var (
+    // key derivation functions
+    oidRSADSI         = asn1.ObjectIdentifier{1, 2, 840, 113549}
+    oidPBES2          = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5, 13}
+
+    // 加密方式
+    oidEncryptionAlgorithm = asn1.ObjectIdentifier{1, 2, 840, 113549, 3}
+    oidDESCBC     = asn1.ObjectIdentifier{1, 3, 14, 3, 2, 7}
+    oidDESEDE3CBC = asn1.ObjectIdentifier{1, 2, 840, 113549, 3, 7}
+
+    oidAES       = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1}
+    oidAES128CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 2}
+    oidAES192CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 22}
+    oidAES256CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 42}
+)
 
 // 结构体数据可以查看以下文档
 // RFC5208 at https://tools.ietf.org/html/rfc5208
@@ -41,78 +51,30 @@ type pbes2Params struct {
     EncryptionScheme  pkix.AlgorithmIdentifier
 }
 
-// pbkdf2 数据
-type pbkdf2Params struct {
-    Salt           []byte
-    IterationCount int
-    PrfParam       pkix.AlgorithmIdentifier `asn1:"optional"`
+// 设置接口
+type KDFOpts interface {
+    DeriveKey(password, salt []byte, size int) (key []byte, params KDFParameters, err error)
+    GetSaltSize() int
+    OID() asn1.ObjectIdentifier
 }
 
-var (
-    // key derivation functions
-    oidRSADSI         = asn1.ObjectIdentifier{1, 2, 840, 113549}
-    oidPKCS5          = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5}
-    oidPKCS5PBKDF2    = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5, 12}
-    oidPBES2          = asn1.ObjectIdentifier{1, 2, 840, 113549, 1, 5, 13}
-
-    // hash 方式
-    oidDigestAlgorithm     = asn1.ObjectIdentifier{1, 2, 840, 113549, 2}
-    oidHMACWithMD5         = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 5}
-    oidHMACWithSHA1        = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 7}
-    oidHMACWithSHA224      = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 8}
-    oidHMACWithSHA256      = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 9}
-    oidHMACWithSHA384      = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 10}
-    oidHMACWithSHA512      = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 11}
-    oidHMACWithSHA512_224  = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 12}
-    oidHMACWithSHA512_256  = asn1.ObjectIdentifier{1, 2, 840, 113549, 2, 13}
-
-    // 加密方式
-    oidEncryptionAlgorithm = asn1.ObjectIdentifier{1, 2, 840, 113549, 3}
-    oidDESCBC     = asn1.ObjectIdentifier{1, 3, 14, 3, 2, 7}
-    oidDESEDE3CBC = asn1.ObjectIdentifier{1, 2, 840, 113549, 3, 7}
-
-    oidAES       = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1}
-    oidAES128CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 2}
-    oidAES192CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 22}
-    oidAES256CBC = asn1.ObjectIdentifier{2, 16, 840, 1, 101, 3, 4, 1, 42}
-)
-
-// HMAC 列表
-var oidAsn1s = map[string]asn1.ObjectIdentifier{
-    "MD5":        oidHMACWithMD5,
-    "SHA1":       oidHMACWithSHA1,
-    "SHA224":     oidHMACWithSHA224,
-    "SHA256":     oidHMACWithSHA256,
-    "SHA384":     oidHMACWithSHA384,
-    "SHA512":     oidHMACWithSHA512,
-    "SHA512_224": oidHMACWithSHA512_224,
-    "SHA512_256": oidHMACWithSHA512_256,
-}
-
-// hash 列表
-var oidHashs = map[string]func() hash.Hash{
-    "MD5":        md5.New,
-    "SHA1":       sha1.New,
-    "SHA224":     sha256.New224,
-    "SHA256":     sha256.New,
-    "SHA384":     sha512.New384,
-    "SHA512":     sha512.New,
-    "SHA512_224": sha512.New512_224,
-    "SHA512_256": sha512.New512_256,
+// 数据接口
+type KDFParameters interface {
+    DeriveKey(password []byte, size int) (key []byte, err error)
 }
 
 // 配置
 type Opts struct {
-    SaltSize       int
-    IterationCount int
-    HMACHash       string
+    KDFOpts KDFOpts
 }
 
 // 默认配置
-var DefaultOpts = &Opts{
-    SaltSize:       PBKDF2SaltSize,
-    IterationCount: PBKDF2Iterations,
-    HMACHash:       "SHA256",
+var DefaultOpts = Opts{
+    KDFOpts: PBKDF2Opts{
+        SaltSize:       PBKDF2SaltSize,
+        IterationCount: PBKDF2Iterations,
+        HMACHash:       crypto.SHA256,
+    },
 }
 
 // PEM 块
@@ -123,11 +85,6 @@ type rfc1423Algo struct {
     keySize    int
     blockSize  int
     identifier asn1.ObjectIdentifier
-}
-
-// 生成 pbkdf2 值
-func (this rfc1423Algo) deriveKey(password, salt []byte, iterationCount int, h func() hash.Hash) []byte {
-    return pbkdf2.Key(password, salt, iterationCount, this.keySize, h)
 }
 
 // PEM 块列表
@@ -181,61 +138,9 @@ func AppendOID(b asn1.ObjectIdentifier, v ...int) asn1.ObjectIdentifier {
     return append(n, v...)
 }
 
-// 添加 oidAsn1
-func AddOidAsn1(name string, identifier asn1.ObjectIdentifier) {
-    if _, ok := oidAsn1s[name]; ok {
-        delete(oidAsn1s, name)
-    }
-
-    oidAsn1s[name] = identifier
-}
-
-// 添加 oidHash
-func AddOidHash(name string, value func() hash.Hash) {
-    if _, ok := oidHashs[name]; ok {
-        delete(oidHashs, name)
-    }
-
-    oidHashs[name] = value
-}
-
 // 添加 rfc1423Algo
 func AddRfc1423Algo(value rfc1423Algo) {
     rfc1423Algos = append(rfc1423Algos, value)
-}
-
-// 返回使用的 Hash 方式
-func prfByOID(oid asn1.ObjectIdentifier) func() hash.Hash {
-    if len(oid) == 0 {
-        return md5.New
-    }
-
-    if oid.Equal(oidHMACWithMD5) {
-        return md5.New
-    }
-    if oid.Equal(oidHMACWithSHA1) {
-        return sha1.New
-    }
-    if oid.Equal(oidHMACWithSHA224) {
-        return sha256.New224
-    }
-    if oid.Equal(oidHMACWithSHA256) {
-        return sha256.New
-    }
-    if oid.Equal(oidHMACWithSHA384) {
-        return sha512.New384
-    }
-    if oid.Equal(oidHMACWithSHA512) {
-        return sha512.New
-    }
-    if oid.Equal(oidHMACWithSHA512_224) {
-        return sha512.New512_224
-    }
-    if oid.Equal(oidHMACWithSHA512_256) {
-        return sha512.New512_256
-    }
-
-    return nil
 }
 
 func cipherByKey(key x509.PEMCipher) *rfc1423Algo {
@@ -245,6 +150,18 @@ func cipherByKey(key x509.PEMCipher) *rfc1423Algo {
             return alg
         }
     }
+
+    return nil
+}
+
+func cipherById(id asn1.ObjectIdentifier) *rfc1423Algo {
+    for i := range rfc1423Algos {
+        alg := &rfc1423Algos[i]
+        if alg.identifier.Equal(id) {
+            return alg
+        }
+    }
+
     return nil
 }
 
@@ -288,45 +205,25 @@ func DecryptPKCS8PrivateKey(data, password []byte) ([]byte, error) {
         return nil, errors.New("pkcs8: invalid PBES2 iv")
     }
 
+    // 解析出数据
     var kdfParam pbkdf2Params
     if _, err := asn1.Unmarshal(params.KeyDerivationFunc.Parameters.FullBytes, &kdfParam); err != nil {
         return nil, errors.New("pkcs8: invalid PBES2 parameters")
     }
 
-    salt := kdfParam.Salt
-    iter := kdfParam.IterationCount
-
-    // pbkdf2 hash function
-    keyHash := prfByOID(kdfParam.PrfParam.Algorithm)
-    if keyHash == nil {
-        return nil, errors.New("unsupported PRF")
+    ciph := cipherById(params.EncryptionScheme.Algorithm)
+    if ciph == nil {
+        return nil, errors.New(fmt.Sprintf("unsupported encrypted PEM: unknown algorithm %v", params.EncryptionScheme.Algorithm))
     }
 
-    var symkey []byte
-    var block cipher.Block
-    var err error
-    switch {
-        // AES-128-CBC, AES-192-CBC, AES-256-CBC
-        case params.EncryptionScheme.Algorithm.Equal(oidAES128CBC):
-            symkey = pbkdf2.Key(password, salt, iter, 16, keyHash)
-            block, err = aes.NewCipher(symkey)
-        case params.EncryptionScheme.Algorithm.Equal(oidAES192CBC):
-            symkey = pbkdf2.Key(password, salt, iter, 24, keyHash)
-            block, err = aes.NewCipher(symkey)
-        case params.EncryptionScheme.Algorithm.Equal(oidAES256CBC):
-            symkey = pbkdf2.Key(password, salt, iter, 32, keyHash)
-            block, err = aes.NewCipher(symkey)
-        // DES, TripleDES
-        case params.EncryptionScheme.Algorithm.Equal(oidDESCBC):
-            symkey = pbkdf2.Key(password, salt, iter, 8, keyHash)
-            block, err = des.NewCipher(symkey)
-        case params.EncryptionScheme.Algorithm.Equal(oidDESEDE3CBC):
-            symkey = pbkdf2.Key(password, salt, iter, 24, keyHash)
-            block, err = des.NewTripleDESCipher(symkey)
-        default:
-            return nil, errors.New(fmt.Sprintf("unsupported encrypted PEM: unknown algorithm %v", params.EncryptionScheme.Algorithm))
+    // AES-128-CBC, AES-192-CBC, AES-256-CBC
+    // DES, TripleDES
+    symkey, err := kdfParam.DeriveKey(password, ciph.keySize)
+    if err != nil {
+        return nil, err
     }
 
+    block, err := ciph.cipherFunc(symkey)
     if err != nil {
         return nil, err
     }
@@ -374,48 +271,35 @@ func EncryptPKCS8PrivateKey(
         return nil, errors.New(fmt.Sprintf("failed to encrypt PEM: unknown algorithm %v", alg))
     }
 
-    var opt any
+    var kdfOpt any
     if len(opts) > 0 {
-        opt = opts[0]
+        kdfOpt = opts[0]
     } else {
-        opt = DefaultOpts
+        kdfOpt = DefaultOpts
     }
 
-    var oidhash string
-    var saltSize int
-    var iterationCount int
+    var opt *Opts
 
     // 断言类型
-    switch opt.(type) {
+    switch optData := kdfOpt.(type) {
         case Opts:
-            optData := opt.(Opts)
-
-            oidhash = optData.HMACHash
-            saltSize = optData.SaltSize
-            iterationCount = optData.IterationCount
+            opt = &optData
         case string:
-            oidhash = opt.(string)
-            saltSize = DefaultOpts.SaltSize
-            iterationCount = DefaultOpts.IterationCount
-        default:
-            oidhash = DefaultOpts.HMACHash
-            saltSize = DefaultOpts.SaltSize
-            iterationCount = DefaultOpts.IterationCount
+            h, err := GetHash(optData)
+            if err != nil {
+                return nil, err
+            }
+
+            opt = &Opts{
+                KDFOpts: PBKDF2Opts{
+                    SaltSize:       PBKDF2SaltSize,
+                    IterationCount: PBKDF2Iterations,
+                    HMACHash:       h,
+                },
+            }
     }
 
-    // 签名方式
-    oidUseHash := sha1.New
-    if selectedHash, ok := oidHashs[oidhash]; ok {
-        oidUseHash = selectedHash
-    }
-
-    // ObjectIdentifier 方式
-    oidUseAsn1 := oidHMACWithSHA1
-    if selectedAsn1, ok := oidAsn1s[oidhash]; ok {
-        oidUseAsn1 = selectedAsn1
-    }
-
-    salt := make([]byte, saltSize)
+    salt := make([]byte, opt.KDFOpts.GetSaltSize())
     if _, err := io.ReadFull(rand, salt); err != nil {
         return nil, errors.New(err.Error() + " failed to generate salt")
     }
@@ -425,7 +309,11 @@ func EncryptPKCS8PrivateKey(
         return nil, errors.New(err.Error() + " failed to generate IV")
     }
 
-    key := ciph.deriveKey(password, salt, iterationCount, oidUseHash)
+    key, kdfParams, err := opt.KDFOpts.DeriveKey(password, salt, ciph.keySize)
+    if err != nil {
+        return nil, err
+    }
+
     block, err := ciph.cipherFunc(key)
     if err != nil {
         return nil, errors.New(err.Error() + " failed to create cipher")
@@ -443,17 +331,6 @@ func EncryptPKCS8PrivateKey(
     enc.CryptBlocks(encrypted, encrypted)
 
     // 生成 asn1 数据开始
-    kdfParams := pbkdf2Params{
-        Salt:           salt,
-        IterationCount: iterationCount,
-        PrfParam: pkix.AlgorithmIdentifier{
-            Algorithm:  oidUseAsn1,
-            Parameters: asn1.RawValue{
-                Tag: asn1.TagNull,
-            },
-        },
-    }
-
     marshalledParams, err := asn1.Marshal(kdfParams)
     if err != nil {
         return nil, err

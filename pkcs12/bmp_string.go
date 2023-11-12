ce01d61aad9d46823cb8307bd3adbf4a8a351575
deatil <2217957370@qq.com>
2023-11-12 21:28:51 +0800

    添加 PKCS12 证书 BER 编码解析

--- pkcs12/bmp_string.go
+++ pkcs12/bmp_string.go
@@ -5,9 +5,12 @@ import (
 )
 
 var (
+    // BmpStringZeroTerminated returns s encoded in UCS-2 with a zero terminator.
     bmpStringZeroTerminated = tool.BmpStringZeroTerminated
 
+    // BmpString returns s encoded in UCS-2
     bmpString = tool.BmpString
 
+    // DecodeBMPString return utf-8 string
     decodeBMPString = tool.DecodeBMPString
 )

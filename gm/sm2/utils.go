16299353b617c1b0912399158439d0cd81464840
deatil <2217957370@qq.com>
2024-01-30 12:44:32 +0800

    更改 SM2 公钥压缩函数接口

--- gm/sm2/utils.go
+++ gm/sm2/utils.go
@@ -1,21 +1,28 @@
 package sm2
 
 import (
+    "errors"
+
     "github.com/deatil/go-cryptobin/gm/sm2/curve"
 )
 
 // 解缩公钥
 // Decompress PublicKey data
-func Decompress(data []byte) *PublicKey {
+func Decompress(data []byte) (*PublicKey, error) {
     c := P256()
 
     x, y := curve.UnmarshalCompressed(c, data)
+    if x == nil || y == nil {
+        return nil, errors.New("cryptobin/sm2: compress publicKey is incorrect.")
+    }
 
-    return &PublicKey{
+    pub := &PublicKey{
         Curve: c,
         X:     x,
         Y:     y,
     }
+
+    return pub, nil
 }
 
 // 压缩公钥

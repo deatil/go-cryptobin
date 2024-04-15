12033c071cbf81ce97b09e4efb9882a2a00a010c
deatil <2217957370@qq.com>
2024-04-15 13:22:36 +0800

    更新

--- elliptic/nums/nums.go
+++ elliptic/nums/nums.go
@@ -23,7 +23,7 @@ var (
 var (
     once                   sync.Once
     p256d1, p384d1, p512d1 *elliptic.CurveParams
-    p256t1, p512t1         *elliptic.CurveParams
+    p256t1, p384t1, p512t1 *rcurve
 )
 
 func bigFromHex(s string) (i *big.Int) {
@@ -38,16 +38,17 @@ func initAll() {
     initP512d1()
 
     initP256t1()
+    initP384t1()
     initP512t1()
 }
 
 func initP256d1() {
     p256d1 = &elliptic.CurveParams{
-        P: bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff43"),
-        N: bigFromHex("ffffffffffffffffffffffffffffffffe43c8275ea265c6020ab20294751a825"),
+        P: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF43"),
+        N: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFE43C8275EA265C6020AB20294751A825"),
         B: bigFromHex("25581"),
         Gx: bigFromHex("01"),
-        Gy: bigFromHex("696f1853c1e466d7fc82c96cceeedd6bd02c2f9375894ec10bf46306c2b56c77"),
+        Gy: bigFromHex("696F1853C1E466D7FC82C96CCEEEDD6BD02C2F9375894EC10BF46306C2B56C77"),
         BitSize: 256,
         Name: "numsp256d1",
     }
@@ -55,11 +56,11 @@ func initP256d1() {
 
 func initP384d1() {
     p384d1 = &elliptic.CurveParams{
-        P: bigFromHex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffec3"),
-        N: bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffd61eaf1eeb5d6881beda9d3d4c37e27a604d81f67b0e61b9"),
-        B: bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff77bb"),
+        P: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEC3"),
+        N: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFD61EAF1EEB5D6881BEDA9D3D4C37E27A604D81F67B0E61B9"),
+        B: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF77BB"),
         Gx: bigFromHex("02"),
-        Gy: bigFromHex("3c9f82cb4b87b4dc71e763e0663e5dbd8034ed422f04f82673330dc58d15ffa2b4a3d0bad5d30f865bcbbf503ea66f43"),
+        Gy: bigFromHex("3C9F82CB4B87B4DC71E763E0663E5DBD8034ED422F04F82673330DC58D15FFA2B4A3D0BAD5D30F865BCBBF503EA66F43"),
         BitSize: 384,
         Name: "numsp384d1",
     }
@@ -67,11 +68,11 @@ func initP384d1() {
 
 func initP512d1() {
     p512d1 = &elliptic.CurveParams{
-        P: bigFromHex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdc7"),
-        N: bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff5b3ca4fb94e7831b4fc258ed97d0bdc63b568b36607cd243ce153f390433555d"),
-        B: bigFromHex("1d99b"),
+        P: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFDC7"),
+        N: bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5B3CA4FB94E7831B4FC258ED97D0BDC63B568B36607CD243CE153F390433555D"),
+        B: bigFromHex("1D99B"),
         Gx: bigFromHex("02"),
-        Gy: bigFromHex("1c282eb23327f9711952c250ea61ad53fcc13031cf6dd336e0b9328433afbdd8cc5a1c1f0c716fdc724dde537c2b0adb00bb3d08dc83755b205cc30d7f83cf28"),
+        Gy: bigFromHex("1C282EB23327F9711952C250EA61AD53FCC13031CF6DD336E0B9328433AFBDD8CC5A1C1F0C716FDC724DDE537C2B0ADB00BB3D08DC83755B205CC30D7F83CF28"),
         BitSize: 512,
         Name: "numsp512d1",
     }
@@ -98,33 +99,57 @@ func P512d1() elliptic.Curve {
 // ============
 
 func initP256t1() {
-    p256t1 = &elliptic.CurveParams{
-        P: bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff43"),
-        N: bigFromHex("3fffffffffffffffffffffffffffffffbe6aa55ad0a6bc64e5b84e6f1122b4ad"),
-        B: bigFromHex("3bee"),
-        Gx: bigFromHex("0d"),
-        Gy: bigFromHex("7d0ab41e2a1276dba3d330b39fa046bfbe2a6d63824d303f707f6fb5331cadba"),
-        BitSize: 256,
-        Name: "numsp256t1",
+    twisted := p256d1
+    params := &elliptic.CurveParams{
+        Name:    "numsp256t1",
+        P:       twisted.P,
+        N:       twisted.N,
+        BitSize: twisted.BitSize,
     }
+    params.Gx = bigFromHex("0D")
+    params.Gy = bigFromHex("7D0AB41E2A1276DBA3D330B39FA046BFBE2A6D63824D303F707F6FB5331CADBA")
+    r := bigFromHex("3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFBE6AA55AD0A6BC64E5B84E6F1122B4AD")
+    p256t1 = newRcurve(twisted, params, r)
+}
+
+func initP384t1() {
+    twisted := p384d1
+    params := &elliptic.CurveParams{
+        Name:    "numsp384t1",
+        P:       twisted.P,
+        N:       twisted.N,
+        BitSize: twisted.BitSize,
+    }
+    params.Gx = bigFromHex("08")
+    params.Gy = bigFromHex("749CDABA136CE9B65BD4471794AA619DAA5C7B4C930BFF8EBD798A8AE753C6D72F003860FEBABAD534A4ACF5FA7F5BEE")
+    r := bigFromHex("3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFECD7D11ED5A259A25A13A0458E39F4E451D6D71F70426E25")
+    p384t1 = newRcurve(twisted, params, r)
 }
 
 func initP512t1() {
-    p512t1 = &elliptic.CurveParams{
-        P: bigFromHex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdc7"),
-        N: bigFromHex("3fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa7e50809efdabbb9a624784f449545f0dcea5ff0cb800f894e78d1cb0b5f0189"),
-        B: bigFromHex("9baa8"),
-        Gx: bigFromHex("20"),
-        Gy: bigFromHex("7d67e841dc4c467b605091d80869212f9ceb124bf726973f9ff048779e1d614e62ae2ece5057b5dad96b7a897c1d72799261134638750f4f0cb91027543b1c5e"),
-        BitSize: 512,
-        Name: "numsp512t1",
+    twisted := p512d1
+    params := &elliptic.CurveParams{
+        Name:    "numsp512t1",
+        P:       twisted.P,
+        N:       twisted.N,
+        BitSize: twisted.BitSize,
     }
+    params.Gx = bigFromHex("20")
+    params.Gy = bigFromHex("7D67E841DC4C467B605091D80869212F9CEB124BF726973F9FF048779E1D614E62AE2ECE5057B5DAD96B7A897C1D72799261134638750F4F0CB91027543B1C5E")
+    r := bigFromHex("3FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA7E50809EFDABBB9A624784F449545F0DCEA5FF0CB800F894E78D1CB0B5F0189")
+    p512t1 = newRcurve(twisted, params, r)
 }
 
 // P256t1() returns a Curve which implements p256t1 of Microsoft's Nothing Up My Sleeve (NUMS)
 func P256t1() elliptic.Curve {
     once.Do(initAll)
     return p256t1
+}
+
+// P384t1() returns a Curve which implements p384t1 of Microsoft's Nothing Up My Sleeve (NUMS)
+func P384t1() elliptic.Curve {
+    once.Do(initAll)
+    return p384t1
 }
 
 // P512t1() returns a Curve which implements p512t1 of Microsoft's Nothing Up My Sleeve (NUMS)
--- elliptic/nums/nums_test.go
+++ elliptic/nums/nums_test.go
@@ -2,6 +2,7 @@ package nums
 
 import (
     "testing"
+    "math/big"
     "crypto/rand"
     "crypto/ecdsa"
     "crypto/elliptic"
@@ -35,8 +36,9 @@ func Test_Brainpool(t *testing.T) {
         {"P384d1", P384d1()},
         {"P512d1", P512d1()},
 
-        // {"P256t1", P256t1()},
-        // {"P512t1", P512t1()},
+        {"P256t1", P256t1()},
+        {"P384t1", P384t1()},
+        {"P512t1", P512t1()},
     }
 
     for _, c := range tests {
@@ -44,4 +46,111 @@ func Test_Brainpool(t *testing.T) {
             testCurve(t, c.curve)
         })
     }
+}
+
+// ==========
+
+func get_paddQ(x1, y1, x2, y2, a, p *big.Int) (*big.Int, *big.Int) {
+    var member, denomintor, tmp big.Int
+    var flag bool
+
+    zero := new(big.Int).SetInt64(0)
+    two := new(big.Int).SetInt64(2)
+    three := new(big.Int).SetInt64(3)
+
+    flag = true
+
+    if x1.Cmp(x2) == 0 && y1.Cmp(y2) == 0 {
+        member.Mul(three, x1)
+        member.Mul(&member, x1)
+        member.Add(&member, a)
+
+        denomintor.Mul(two, y1)
+    } else {
+        member.Sub(y2, y1)
+        denomintor.Sub(x2, x1)
+
+        tmp.Mul(&member, &denomintor)
+        if tmp.Cmp(zero) == -1 {
+            flag = false
+
+            member.Abs(&member)
+            denomintor.Abs(&denomintor)
+        }
+    }
+
+    var gcd big.Int
+    gcd.GCD(nil, nil, &member, &denomintor)
+
+    var inverse_deno big.Int
+    inverse_deno.ModInverse(&denomintor, p)
+
+    var k big.Int
+    k.Mul(&member, &inverse_deno)
+
+    if !flag {
+        k.Neg(&k)
+    }
+
+    k.Mod(&k, p)
+
+    var x3, y3 big.Int
+    x3.Mul(&x3, &k)
+    x3.Mul(&x3, &k)
+    x3.Sub(&x3, x1)
+    x3.Sub(&x3, x2)
+    x3.Mod(&x3, p)
+
+    y3.Sub(x1, &x3)
+    y3.Mul(&y3, &k)
+    y3.Sub(&y3, y1)
+    y3.Mod(&y3, p)
+
+    return &x3, &y3
+}
+
+func get_order(x0, y0, a, b, p *big.Int) (r *big.Int) {
+    var x1, y1 big.Int
+    var temp_x, temp_y big.Int
+    var n big.Int
+
+    one := new(big.Int).SetInt64(1)
+
+    x1.Set(x0)
+    y1.Set(one)
+    y1.Neg(&y1)
+    y1.Mul(&y1, y0)
+    y1.Mod(&y1, p)
+
+    temp_x.Set(x0)
+    temp_y.Set(y0)
+
+    n.Set(one)
+
+    var xp, yp *big.Int
+    for {
+        n.Add(&n, one)
+        xp, yp = get_paddQ(&temp_x, &temp_y, x0, y0, a, p)
+
+        if xp.Cmp(&x1) == 0 && yp.Cmp(&y1) == 0 {
+            r = n.Add(&n, one)
+            return
+        }
+
+        temp_x.Set(xp)
+        temp_y.Set(yp)
+    }
+
+    return
+}
+
+func test_GetN(t *testing.T) {
+    x0 := bigFromHex("02")
+    y0 := bigFromHex("3c9f82cb4b87b4dc71e763e0663e5dbd8034ed422f04f82673330dc58d15ffa2b4a3d0bad5d30f865bcbbf503ea66f43")
+    p := bigFromHex("fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffec3")
+    a := bigFromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEC0")
+    b := bigFromHex("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff77bb")
+
+    n := get_order(x0, y0, a, b, p)
+    t.Errorf("%x", n.Bytes())
 }

348d5e83acbe7177e0045c3ade0cd536883491a7
deatil <2217957370@qq.com>
2025-01-10 23:54:21 +0800

    openssh update

--- ssh/key_setting.go
+++ ssh/key_setting.go
@@ -12,30 +12,30 @@ import (
 
 func init() {
     AddKey(GetStructName(&rsa.PrivateKey{}), func() Key {
-        return new(KeyRsa)
+        return new(KeyRSA)
     })
     AddKey(ssh.KeyAlgoRSA, func() Key {
-        return new(KeyRsa)
+        return new(KeyRSA)
     })
 
     AddKey(GetStructName(&ecdsa.PrivateKey{}), func() Key {
-        return new(KeyEcdsa)
+        return new(KeyECDSA)
     })
     AddKey(ssh.KeyAlgoECDSA256, func() Key {
-        return new(KeyEcdsa)
+        return new(KeyECDSA)
     })
     AddKey(ssh.KeyAlgoECDSA384, func() Key {
-        return new(KeyEcdsa)
+        return new(KeyECDSA)
     })
     AddKey(ssh.KeyAlgoECDSA521, func() Key {
-        return new(KeyEcdsa)
+        return new(KeyECDSA)
     })
 
     AddKey(GetStructName(ed25519.PrivateKey{}), func() Key {
-        return new(KeyEdDsa)
+        return new(KeyEdDSA)
     })
     AddKey(ssh.KeyAlgoED25519, func() Key {
-        return new(KeyEdDsa)
+        return new(KeyEdDSA)
     })
 
     AddKey(GetStructName(&sm2.PrivateKey{}), func() Key {

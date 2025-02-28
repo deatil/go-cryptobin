3e99c2cc3f3efca10ac38d66e7adc8fbaeb46c62
deatil <2217957370@qq.com>
2025-02-28 13:15:25 +0800

    更新

--- cryptobin/ecdsa/ecdsa_test.go
+++ cryptobin/ecdsa/ecdsa_test.go
@@ -4,8 +4,15 @@ import (
     "testing"
     "crypto/rand"
 
+    "github.com/deatil/go-cryptobin/pubkey/ecdsa"
+    "github.com/deatil/go-cryptobin/elliptic/secp256k1"
+
     cryptobin_test "github.com/deatil/go-cryptobin/tool/test"
 )
+
+func init() {
+    ecdsa.AddNamedCurve(secp256k1.S256(), secp256k1.OIDNamedCurveSecp256k1)
+}
 
 var (
     prikeyRC2_40En = `
@@ -317,6 +324,28 @@ func Test_PrivateKey_Bytes(t *testing.T) {
     assertNotEmpty(d, "PrivateKey_Bytes")
 
     xk := New().SetCurve("P256").FromPrivateKeyBytes(d)
+
+    assertNoError(xk.Error(), "PrivateKey_Bytes-xk")
+
+    assertEqual(xk.GetPrivateKey(), obj.GetPrivateKey(), "PrivateKey_Bytes-xk")
+}
+
+func Test_PrivateKey_Bytes_2(t *testing.T) {
+    assertNoError := cryptobin_test.AssertNoErrorT(t)
+    assertEqual := cryptobin_test.AssertEqualT(t)
+    assertNotEmpty := cryptobin_test.AssertNotEmptyT(t)
+
+    obj := New().WithCurve(secp256k1.S256()).GenerateKey()
+
+    assertNoError(obj.Error(), "PrivateKeyD")
+
+    priv := obj.GetPrivateKey()
+
+    d := priv.D.Bytes()
+
+    assertNotEmpty(d, "PrivateKey_Bytes")
+
+    xk := New().WithCurve(secp256k1.S256()).FromPrivateKeyBytes(d)
 
     assertNoError(xk.Error(), "PrivateKey_Bytes-xk")
 
